0d0c891d6342ae32177988a642523937018287f7
Sam <90224546+qwoza@users.noreply.github.com>
2021-11-13 19:08:34 +0000

    Updated Player.java
    Added constructor + getters and setters

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,38 @@
 
 public class Player {
+	private int playerID;
+	private int maxLevelID;
+	private String playerName;
+	private String playerRecord;
 
+	public Player(int playerID, String playerName, int maxLevelID) {
+		this.playerID = playerID;
+		this.playerName = playerName;
+		this.maxLevelID = maxLevelID;
+	}
+
+	public int getPlayerID() {
+		return this.playerID;
+	}
+
+	public void setPlayerID(int playerID) {
+		this.playerID = playerID; 
+	}
+
+	public int getMaxLevelID() {
+		return this.maxLevelID;
+	}
+
+	public void setMaxLevelID(int maxLevelID) {
+		this.maxLevelID = maxLevelID;
+	}
+
+	public String getPlayerName() {
+		return this.playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerNamer = playerName;
+	}
+	
 }
