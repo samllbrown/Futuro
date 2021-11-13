65be3994d10d5a4be2f9d01965d5e99032cbabbc
Sam <90224546+qwoza@users.noreply.github.com>
2021-11-13 18:51:27 +0000

    Item Constructor
    Constructor for Item class

--- src/gameObject/Item.java
+++ src/gameObject/Item.java
@@ -2,10 +2,18 @@ package gameObject;
 
 
 public abstract class Item {
-	
 	private String itemID;
 	private int xPos; 
 	private int yPos; 
 	private int xRange;
 	private int yRange;
+	
+	public abstract Item(String itemID, int xPos, int yPos, int xRange, int yRange) {
+		this.itemID = itemID;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.xRange = xRange;
+		this.yRange = yRange;
+	}
+	
 }
--- src/gameObject/Lightning.java
+++ src/gameObject/Lightning.java
@@ -3,7 +3,6 @@ package gameObject;
 public class Lightning extends Item {
 
 	public Lightning() {
-	
 	}
 
 }
