5fbd3355f01589eb9f7bbf3bb5360b69f8fea9f5
EvolveAir <samuellbrown1@gmail.com>
2021-11-15 19:51:43 +0000

    Updated Game.java

--- src/managers/Game.java
+++ src/managers/Game.java
@@ -1,5 +1,21 @@
 package managers;
+
+import board.Level;
+
 public class Game {
-	public Game() {		
+	
+	private Level level;
+	
+	
+	public Game(Level level) {	
+		setLevel(level);
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+
+	public void setLevel(Level level) {
+		this.level = level;
 	}
 }
