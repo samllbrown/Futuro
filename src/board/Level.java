ed6d59a1d8adbb25cabead55b1bcd3c2f296de95
ZeTaaT <78171648+ZeTaaT@users.noreply.github.com>
2021-11-23 21:20:28 +0100

    Added imports
    Imports so Level works

--- src/board/Level.java
+++ src/board/Level.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+import board.Path;
+import board.Tile;
+import board.Tunnel;
+import board.Wall;
+
 import board.Grid;
 import gameObject.GameObjectFactory;
 import gameObject.Item;
