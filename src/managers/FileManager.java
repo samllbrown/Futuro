0d1f71cbbd986389fd851e30bc3845126207e1b4
Sam <90224546+qwoza@users.noreply.github.com>
2021-11-25 16:28:31 +0000

    UPDATED THE PLAYER READER
    I HAVE UPDATED THE PLAYER READER METHOD AND IT SHOULD NOW WORK FOR PLAYER RECORDS WITH THE FOLLOWING FORMAT:
    PLAYERID,PLAYERNAME,MAXLEVELID

--- src/managers/FileManager.java
+++ src/managers/FileManager.java
@@ -68,26 +68,43 @@ public class FileManager {
 	 * @return
 	 * @throws FileNotFoundException
 	 */
-	public static Player readPlayerFile (int playerID) throws FileNotFoundException {
-		Player selectedPlayer = null;
-		Scanner in = new Scanner(PLAYER_FILE);
-		ArrayList<Player> playersFromFile = new ArrayList<>();
-		while (in.hasNextLine()) {
-			String curLine = in.nextLine();
-			Scanner line = new Scanner(curLine).useDelimiter(",");
-			playersFromFile.add(new Player(line.nextInt(), line.next(), line.nextInt()));
-		}
-		for (Player p : playersFromFile) {
-			if (p.getPlayerID() == playerID) {
-				selectedPlayer = p;
+	public static Player readPlayerFile (int playerID) throws IOException {
+		String currentLine;
+		String playerRecord = null;
+		BufferedReader br = new BufferedReader(new FileReader(PLAYER_FILE));
+		boolean found = false;
+		int currentPlayerID;
+		while((currentLine = br.readLine()) != null && (!found)) {
+			if (Integer.valueOf(currentLine.split(",")[0]) == playerID) {
+				playerRecord = currentLine;
+				found = true;
 			}
 		}
+		// HELLO DAVID
+		// PLEASE COULD YOU WRITE SOME VALIDATION STUFF FOR THIS METHOD
+		// THANK YOU
+		// SAM
+		Player player = new Player(playerRecord);
+		return player;
+//		Player selectedPlayer = null;
+//		Scanner in = new Scanner(PLAYER_FILE);
+//		ArrayList<Player> playersFromFile = new ArrayList<>();
+//		while (in.hasNextLine()) {
+//			String curLine = in.nextLine();
+//			Scanner line = new Scanner(curLine).useDelimiter(",");
+//			playersFromFile.add(new Player(line.nextInt(), line.next(), line.nextInt()));
+//		}
+//		for (Player p : playersFromFile) {
+//			if (p.getPlayerID() == playerID) {
+//				selectedPlayer = p;
+//			}
+//		}
 		/*try {
 			return selectedPlayer;
 		} catch (NullPointerException e){
 			System.out.println("Player doesn't exist!");
 		}*/
-		return selectedPlayer;
+//		return selectedPlayer;
 	}
 
 	/**
--- src/managers/Player.java
+++ src/managers/Player.java
@@ -12,7 +12,22 @@ public class Player {
 		this.playerName = playerName;
 		this.maxLevelID = maxLevelID;
 		this.playerID = Player.nextPlayerID;
-		Player.nextPlayerID++;
+		Player.nextPlayerID++; // WHAT DO YOU MEAN WHY IS THIS HERE
+	}
+
+
+	// HERE WE ARE DEFINING A DIFFERENT SORT OF CONSTRUCTOR WHICH I PERSONALLY FIND QUITE RIVETTING
+	// PERHAPS WE COULD DO THIS FOR OTHER SORTS OF OBJECTS AS WELL
+	// I THINK THIS IS A GOOD IDEA POSSIBLY
+	// PLEASE COULD ANYONE READING THIS COMMENT GET BACK TO ME ON THEIR THOUGHTS ABOUT THIS
+	// MANY THANKS
+	// SAM R
+	// XOXOXOXOXOXOXOXOXOXO
+	public Player(String playerRecord) {
+		String[] fields = playerRecord.split(",");
+		this.playerID = Integer.valueOf(fields[0]);
+		this.playerName = fields[1];
+		this.maxLevelID = Integer.valueOf(fields[2]);
 	}
 
 	public String getPlayerRecord() {
