bedc9e4218a06a6ba0d333dff0c068771e8e4154
ZeTaaT <78171648+ZeTaaT@users.noreply.github.com>
2021-11-29 21:48:15 +0100

    Updated Delete Player (Doesn't work)

--- src/managers/FileManager.java
+++ src/managers/FileManager.java
@@ -159,7 +159,7 @@ public class FileManager {
 		HashSet<Integer> playerids = getAllIdsInFile(PLAYER_FILE);
 		System.out.println(playerids.contains(player.getPlayerID()));
 		if(!(playerids.contains(player.getPlayerID()))) {
-			deleteRecordWithID(player.getPlayerID(), PLAYER_FILE);
+			//deletes statement here
 			deleted = true;
 		}
 		return deleted;
