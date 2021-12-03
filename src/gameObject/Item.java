18748dac704e51d1a503cba70e50b7d0ce19e04b
EvolveAir <samuellbrown1@gmail.com>
2021-12-03 15:14:46 +0000

    Item javadoc

--- src/gameObject/Item.java
+++ src/gameObject/Item.java
@@ -1,21 +1,40 @@
 package gameObject;
 
 import javafx.scene.image.Image;
-import org.w3c.dom.css.Rect;
-
 import java.awt.*;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Item.
+ */
 public abstract class Item extends Rectangle {
-	//private String itemID;
+
+	/** The y. */
 	private int x, y;
 
+	/** The x range of the item. */
 	private int xRange;
+
+	/** The y range of the item. */
 	private int yRange;
+
+	/** The x range of the item. */
 	private int damage;
-	protected int uses;
 	
+	/** The uses. */
+	protected int uses;
+
+	/** The is ready for destroy. */
 	public boolean isReadyForDestroy = false;
 
+/**
+ * Instantiates a new item.
+ *
+ * @param x the x
+ * @param y the y
+ * @param damage the damage
+ * @param uses the uses
+ */
 //	protected Item(String itemID, int xPos, int yPos, int xRange, int yRange) {
 	protected Item(int x, int y, int damage, int uses) {
 		this.x = x;
@@ -23,38 +42,84 @@ public abstract class Item extends Rectangle {
 		this.damage = damage;
 		this.uses = uses;
 	}
-	
+
+	/**
+	 * Sets the x range.
+	 *
+	 * @param xRange the new x range
+	 */
 	public void setXRange(int xRange) {
 		this.xRange = xRange;
 	}
-	
+
+	/**
+	 * Sets the y range.
+	 *
+	 * @param yRange the new y range
+	 */
 	public void setYRange(int yRange) {
 		this.yRange = yRange;
 	}
-	
+
+	/**
+	 * Gets the x range.
+	 *
+	 * @return the x range
+	 */
 	public int getXRange() {
 		return this.xRange;
 	}
-	
+
+	/**
+	 * Gets the y range.
+	 *
+	 * @return the y range
+	 */
 	public int getYRange() {
 		return this.yRange;
 	}
-	
+
+	/**
+	 * Gets the grid X.
+	 *
+	 * @return the grid X
+	 */
 	public int getGridX() {
 		return this.x;
 	}
 
+	/**
+	 * Gets the grid Y.
+	 *
+	 * @return the grid Y
+	 */
 	public int getGridY() {
 		return this.y;
 	}
+
+	/**
+	 * Gets the image of the item.
+	 *
+	 * @return the image
+	 */
 	public abstract Image getImage();
 
+	/**
+	 * Checks if is ready for destroy.
+	 *
+	 * @return true, if is ready for destroy
+	 */
 	public boolean isReadyForDestroy() {
 		return (this.uses == 0);
 	}
 
+	/**
+	 * Act - This performs an action on a mech
+	 * from an item
+	 * @param someMech the inputed mech
+	 */
 	public void act(Mech someMech) {
-		if(this.uses != 0) {
+		if (this.uses != 0) {
 			someMech.takeDamage(this.damage);
 			this.uses--;
 		} else {
