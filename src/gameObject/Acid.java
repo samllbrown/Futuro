223d8ca281b07915c96b863697cb87d2c28c1f53
ZeTaaT <78171648+ZeTaaT@users.noreply.github.com>
2021-12-04 19:39:53 +0100

    Squashed commit of the following:
    commit 9598f99d656b82f2fa5ecc8737581e67afd4dcfe
    Author: Debbie <93946607+DebbieL97@users.noreply.github.com>
    Date:   Wed Dec 1 22:03:59 2021 +0000
    
        Update Acid.java

--- src/gameObject/Acid.java
+++ src/gameObject/Acid.java
@@ -4,25 +4,36 @@ import javafx.scene.image.Image;
 
 public class Acid extends Item {
 
-	private static final int X_RANGE = 3;
-	private static final int Y_RANGE = 3;
+	private int xRange = 3;
+	private int yRange = 3;
 	public static final Image ITEM_IMAGE = new Image("file:res/Sprites/acid.png",50, 50, false, false);
-	public static final Image ITEM_IMAGE_PUDDLE = new Image("file:res/Sprites/acidPuddle.png",50, 50, false, false);
-	private int DURATION = 5; //5 ticks or seconds
+	private int duration = 5; //5 ticks or seconds
 	
 	private static int DAMAGE_OVER_TIME_ADULT = 25; //25 Damage per tick
 	private static int DAMAGE_OVER_TIME_BABY = 10; //25 Damage per tick
 	
 	public Acid(int atX, int atY) {
 		super(atX, atY, DAMAGE_OVER_TIME_ADULT);
-		setXRange(X_RANGE);
-		setYRange(Y_RANGE);
+		setXRange(xRange);
+		setYRange(yRange);
 	}
 	
 	@Override
 	public void act(Mech mech) {
-		mech.takeDamage(mech.getIsBaby() ? DAMAGE_OVER_TIME_BABY : DAMAGE_OVER_TIME_ADULT);
-		System.err.println("A MECH HAS TAKEN DAMAGE");
+        while (duration != 0) {
+            mech.takeDamage(mech.getIsBaby() ? DAMAGE_OVER_TIME_BABY : DAMAGE_OVER_TIME_ADULT);
+            System.err.println("A MECH HAS TAKEN DAMAGE");
+            xRange++;
+            yRange++;
+            duration--;
+        }
+        if (duration == 0) {
+            xRange--;
+            yRange--;
+            if ((xRange & yRange) == 0) {
+                //remove the item from the board ???
+            }
+        }
 	}
 
 	@Override
