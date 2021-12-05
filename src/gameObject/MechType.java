e618b74ee0cb8560a80e1e4debfdc1ace4524c27
David-T10 <54846690+David-T10@users.noreply.github.com>
2021-12-05 15:33:03 +0000

    Mech, MechType javadoc

--- src/gameObject/MechType.java
+++ src/gameObject/MechType.java
@@ -1,5 +1,13 @@
 package gameObject;
 
+/**
+ * MechType.java
+ * @author
+ * @version
+ * Last Mod Date:
+ * Description: Enumerates through different Mech types.
+ */
+
 public enum MechType {
     DEATH, RESOURCE, PRODUCTION;
 }
