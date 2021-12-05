508118088dd88c8378b69b8a25ff41227d7600d6
David-T10 <54846690+David-T10@users.noreply.github.com>
2021-12-05 16:31:58 +0000

    Globals javadoc

--- src/services/Globals.java
+++ src/services/Globals.java
@@ -3,6 +3,13 @@ package services;
 import gameObject.Direction;
 import gameObject.MechType;
 
+/**
+ * Globals.java
+ * @author
+ * @version
+ * Last Mod Date:
+ * Description: Contains global constant variables needed.
+ */
 public class Globals {
     public static final String PLAYER_FILE = "players.txt";
     public static final MechType[] NORMAL_MECH_TYPES = {MechType.PRODUCTION, MechType.RESOURCE};
